8996611d3a5655b6e635fb696bc68c5101853c07
Tri-Nguyen <tri.nguyen@coxautoinc.com>
2020-04-11 07:41:41 +0700

    Use static files

--- Startup.cs
+++ Startup.cs
@@ -21,7 +21,10 @@ namespace dot_next
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseWelcomePage();
+            app.UseStaticFiles();
+
+            // app.UseWelcomePage();
+            
             // if (env.IsDevelopment())
             // {
             //     app.UseDeveloperExceptionPage();

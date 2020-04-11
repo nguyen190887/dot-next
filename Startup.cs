7629173d58239e1687b2fb49590032ff21a339c6
Tri-Nguyen <tri.nguyen@coxautoinc.com>
2020-04-11 07:51:28 +0700

    Add mvc

--- Startup.cs
+++ Startup.cs
@@ -16,29 +16,28 @@ namespace dot_next
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(options =>
+            {
+                options.EnableEndpointRouting = false; // need this for .NET Core 3?
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseWelcomePage("/");
+
+            app.UseExceptionHandler("/Home/Error");
+
             app.UseStaticFiles();
 
-            // app.UseWelcomePage();
-            
-            // if (env.IsDevelopment())
-            // {
-            //     app.UseDeveloperExceptionPage();
-            // }
-
-            // app.UseRouting();
-
-            // app.UseEndpoints(endpoints =>
-            // {
-            //     endpoints.MapGet("/", async context =>
-            //     {
-            //         await context.Response.WriteAsync("Hello World!");
-            //     });
-            // });
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}"
+                );
+            });
         }
     }
 }
